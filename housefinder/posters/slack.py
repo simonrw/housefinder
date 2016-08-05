6de1604d07a29650412fa4d5c95aed3bb1f31e2c
Simon Walker <s.r.walker101@googlemail.com>
2016-08-06 00:44:58 +0100

    Add slack poster back in

---
+++ housefinder/posters/slack.py
@@ -0,0 +1,20 @@
+from slackclient import SlackClient
+
+
+class SlackPoster(object):
+
+    def __init__(self, listing, config):
+        self.listing = listing
+        self.client = SlackClient(config['slack']['api_key'])
+        self.channel = config['slack']['channel']
+        self.username = config['slack'].get('username', 'housefinder')
+
+    def post(self):
+        desc = '{}, {} £{}k | {} bedrooms | detail: {} | {}'.format(
+            self.listing.property_type, self.listing.price_modifier_human,
+            self.listing.price_thousands, self.listing.num_bedrooms,
+            self.listing.details_url, self.listing.image_url)
+        self.client.api_call(
+            'chat.postMessage', channel=self.channel, text=desc,
+            username=self.username, icon_emoji=':robot_face:'
+        )
